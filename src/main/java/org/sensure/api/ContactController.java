a1f3eb5becdc7a89c5b750ff9f0b1d7bee51a5c8
JoeyHammoth <137293223+JoeyHammoth@users.noreply.github.com>
2025-03-24 01:52:12 +1100

    fix

--- src/main/java/org/sensure/api/ContactController.java
+++ src/main/java/org/sensure/api/ContactController.java
@@ -3,20 +3,30 @@ package org.sensure.api;
 import org.sensure.api.Contact;
 import org.sensure.api.ContactRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/contacts")
-@CrossOrigin(origins = "https://sensure-react-website.vercel.app")
 public class ContactController {
 
     @Autowired
     private ContactRepo contactRepository;
 
+    @CrossOrigin(origins = "https://sensure-react-website.vercel.app",
+            methods = {RequestMethod.POST, RequestMethod.OPTIONS},
+            allowedHeaders = "*",
+            allowCredentials = "true")
     @PostMapping
     public ResponseEntity<String> submitContact(@RequestBody Contact contact) {
         contactRepository.save(contact);
         return ResponseEntity.ok("Contact saved successfully");
+    }
+
+    @CrossOrigin(origins = "https://sensure-react-website.vercel.app")
+    @RequestMapping(method = RequestMethod.OPTIONS)
+    public ResponseEntity<?> handleOptions() {
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
