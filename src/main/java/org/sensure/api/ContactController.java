05e6735e6469db4349f6492af6d852db5e8e506a
JoeyHammoth <137293223+JoeyHammoth@users.noreply.github.com>
2025-03-24 01:45:47 +1100

    help me

--- src/main/java/org/sensure/api/ContactController.java
+++ src/main/java/org/sensure/api/ContactController.java
@@ -5,35 +5,18 @@ import org.sensure.api.ContactRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/contacts")
-@CrossOrigin(origins = "https://sensure-react-website.vercel.app",
-        methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.OPTIONS},
-        allowedHeaders = {"Authorization", "Content-Type", "Accept"},
-        allowCredentials = "true")
+@CrossOrigin(origins = "https://sensure-react-website.vercel.app")
 public class ContactController {
 
     @Autowired
     private ContactRepo contactRepository;
 
-    @GetMapping
-    public ResponseEntity<List<Contact>> getAllContacts() {
-        return ResponseEntity.ok(contactRepository.findAll());
-    }
-
     @PostMapping
     public ResponseEntity<String> submitContact(@RequestBody Contact contact) {
         contactRepository.save(contact);
         return ResponseEntity.ok("Contact saved successfully");
-    }
-
-    // Handle preflight OPTIONS requests
-    @RequestMapping(method = RequestMethod.OPTIONS)
-    public ResponseEntity<?> handleOptions() {
-        return ResponseEntity.ok().build();
     }
 }
